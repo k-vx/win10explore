ad3e33d01f86a7282b90334777d7ecdebc4d03c4
qwqdanchun <287182701@qq.com>
2020-03-20 21:24:20 +0800

    add 彩蛋

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
     <p>Copyright © 2017-2020 <a href="//<?php echo $_SERVER['SERVER_NAME']; ?>"><?php echo bloginfo('name')?> </a>.
         </p>
+    <p><a style="color: white!important" href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='/wp-content/themes/win10explore/asteroids.min.js';void(0);" title="彩蛋">彩蛋</a> </p>
 </footer>
